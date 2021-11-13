7b0b81f313e8f512a510fc8e92409ee1c11c10b3
C-Birkett <conanbirkett@gmail.com>
2021-11-13 19:16:06 +0000

    13/11/21 changes - created a vector class w/ rotate methods etc. moved everything over to new class

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This is the main file for this project
 
 import numpy as np
-import scipy
+from scipy import linalg as scipyLinalg
 import cmath
 import math
 import matplotlib.pyplot as plt
@@ -168,13 +168,61 @@ def Hamiltonian(k):
 # --- Vectors ---
 
 # Lengths:
-# gamma -> m = 1/2 b (b=reciprocal lattice constant)
+# gamma -> m = 1/2 b (b = reciprocal lattice constant)
 # gamma -> k = b/sqrt(3)
 # k -> m = b/2*sqrt(3)
 
 # reciprocal lattice constant
 RLC = (4*np.pi)/(np.sqrt(3)*PLC)
 
+# 2D rotation matrix
+def rotation_matrix(theta):
+    return np.array([[np.cos(theta), - np.sin(theta)],
+                    [np.sin(theta), np.cos(theta)]])
+
+#define some with exact values to test
+def rotation_matrix_90(theta):
+    return np.array([[0, -1],
+                    [1, 0]])
+
+
+# A simple vector class to cut down on the amount of messy numpy array syntax & to keep methods such as rotation etc in one place
+# these vectors are dimensionless, each element is assumed to be a function of some other value (i.e wavevector k)
+class Vector:
+    def __init__(this, vec: np.array):
+        this.v = np.array(vec)
+    
+    def rotate(this, theta):
+        this.v = np.matmul(rotation_matrix(theta), this.v)
+        return this.v
+
+    def rotate90(this, theta):
+        this.v = np.matmul(rotation_matrix_90(theta), this.v)
+        return this.v
+
+    def len(this):
+        return np.linalg.norm(this.v)
+
+    def k(this, k: np.array):   # evaluate at some wavevector k, elementwise product
+        return np.array([this.v[0] * k[0], this.v[1] * k[1]])
+
+"""
+# testing out the vector class
+test_vector_1 = Vector([1,0])
+test_vector_2 = Vector([0,1])
+print ("\n init test vectors: ", test_vector_1.v, test_vector_2.v)
+
+print("\n types: ", type(test_vector_1), type(test_vector_1.v))
+
+test_vector_1.rotate(np.pi/2)
+test_vector_2.rotate(np.pi/2)
+print ("\n rotated test vectors: ", test_vector_1.v, test_vector_2.v)
+
+print ("\n evaluate test vectors at [0.5,0.5]: ", test_vector_1.k([0.5,0.5]), test_vector_2.k([0.5,0.5]))
+"""
+
+
+""" backup of old lattice vectors (as functions rather than Vector class)
 # Primitive lattice vectors
 def PLV_1(k: np.array):
     return np.array([np.sqrt(3)*PLC*k[0]/2, PLC*k[1]/2])
@@ -191,40 +239,62 @@ def RLV_2(k: np.array):
 
 print("PLC = ", PLC, "\nRLC = ", RLC)
 print("|PLV| = ", np.linalg.norm(PLV_1([1,1])), "\n|RLV| = ", np.linalg.norm(RLV_1([1,1])))
+"""
 
-# Next we define the vectors that describe the paths for the brillouin zone
+# TODO - make a lattice class, then can do lattice.generate_vectors(), lattice.rotate_vectors() etc
+# can even do lattice.find_eigenvalues(path) etc
+# this will make layering and rotating lattices easier. Can even extend functionality to superlattices.
+# for now this will be useful for adding the 2nd layer and then rotating it
 
-def Gamma_to_M(k: np.array):
-    return (1.0/2.0)*RLV_1(k)
+# for simplicity we will define the "default" primitive and reciprocal lattice vectors the same way they were defined before (before weekend of 13/11/21), this can be easily changed in the future if need be
 
-def Gamma_to_K_prime(k: np.array):
-    return (1.0/3.0)*(RLV_1(k)+RLV_2(k))
+# Primitive lattice vectors
+PLV_1 = Vector([np.sqrt(3)*PLC/2, PLC/2])
+PLV_2 = Vector([-np.sqrt(3)*PLC/2, PLC/2])
 
-def M_to_K_prime(k: np.array):
-    return (-1.0/6.0)*RLV_1(k) + (1.0/3.0)*RLV_2(k)
+# Reciprocal lattice vectors
+RLV_1 = Vector([2*np.pi/PLC, 2*np.pi/(np.sqrt(3)*PLC)])
+RLV_2 = Vector([2*np.pi/PLC, -2*np.pi/(np.sqrt(3)*PLC)])
 
-def M_to_K(k: np.array):
-    return (1.0/6.0)*RLV_1(k) - (1.0/3.0)*RLV_2(k)
+# Rotate the reciprocal lattice here:
+RLV_1.rotate90(0)
+RLV_2.rotate90(0)
 
-def Gamma_to_K(k: np.array):
-    return (2.0/3.0)*RLV_1(k) - (1.0/3.0)*RLV_2(k)
+# testing with weird angles
+#RLV_1.rotate(5*np.pi/7)
+#RLV_2.rotate(5*np.pi/7)
 
-def K_to_Gamma(k: np.array):
-    return (-2.0/3.0)*RLV_1(k) + (1.0/3.0)*RLV_2(k)
 
-print("\nK prime to M = ", -M_to_K_prime([1,1])/RLC)
+# Next we define the vectors that describe the paths for the brillouin zone as functions of k
 
-# So for the complete path M -> K -> Gamma -> K' -> M should be the 0 vector
+Gamma_to_M = Vector((1.0/2.0)*RLV_1.v)
 
-#def full_path_vector(k):
-#    return -M_to_K_prime(k) - Gamma_to_K(k) + Gamma_to_K_prime(k) - M_to_K_prime(k)
+Gamma_to_K_prime = Vector((1.0/3.0) * (RLV_1.v + RLV_2.v))
 
-# note K' to M = M to K
-M_to_M = [M_to_K, K_to_Gamma, Gamma_to_K_prime, M_to_K]
+M_to_K_prime = Vector((-1.0/6.0)*RLV_1.v + (1.0/3.0)*RLV_2.v)
 
-#M_to_M = [M_to_K]
+M_to_K = Vector((1.0/6.0)*RLV_1.v - (1.0/3.0)*RLV_2.v)
 
-#print("\nThe complete path M -> K -> Gamma -> K' -> M should be the 0 vector: ", Full_Path([1,1]))
+#to avoid rotating twice
+K_prime_to_M = Vector(M_to_K.v)
+
+Gamma_to_K = Vector((2.0/3.0)*RLV_1.v - (1.0/3.0)*RLV_2.v)
+
+K_to_Gamma = Vector((-2.0/3.0)*RLV_1.v + (1.0/3.0)*RLV_2.v)
+
+M_to_M = [M_to_K, K_to_Gamma, Gamma_to_K_prime, K_prime_to_M]
+
+"""
+# test these make a closed loop - returns 10-7 error in x, which is negligible
+tmp = np.array([0.0,0.0])
+print("\ntmp = ", tmp)
+for v in M_to_M:
+    tmp += v.v
+    print("\nvector = ", v.v)
+    print("\ntmp = ", tmp)
+
+print("this should be equal to zero: ", tmp)
+"""
 
 # conditions ai dot bj = 2Pi delta(i,j)
 # pairwise parallel or perpendicular
@@ -246,66 +316,87 @@ def Figure_d():
     Energy_5 = np.array([])
     Energy_6 = np.array([])
     Energy = [Energy_1,Energy_2,Energy_3,Energy_4,Energy_5,Energy_6]
+    
+    EVec1 = np.array([[]])
+    EVec2 = np.array([[]])
+    EVec3 = np.array([[]])
+    EVec4 = np.array([[]])
+    EVec5 = np.array([[]])
+    EVec6 = np.array([[]])
+    EigenVectors = [EVec1, EVec2, EVec3, EVec4, EVec5, EVec6]
 
     Path_x = np.array([])
     Path_y = np.array([])
     Path = np.array([])
-    EigenVectors = np.array([[],[],[],[],[],[]], dtype=complex)
 
-    Path_Offset = 0
-    #assuming starting from M
-    k_last = Gamma_to_M([1,1])
-    print("\nstarting k offset = ", k_last/RLC)
+    Path_Offset = 0 #for plotting x axis
+    #assuming starting from point M - should rotate with the rest
+    k_last = Gamma_to_M.v
+    #k_last = Gamma_to_M.v
+    print("\nstarting k offset (M) = ", k_last/RLC)
     Path_Offset += np.linalg.norm(k_last)
     print("\ninitial x offset = ", Path_Offset)
 
-    count = 0
     for vectors in M_to_M:
-        vector_length = np.linalg.norm(vectors([1,1]))
+        print("\nmoving along vector: ", vectors.v)
         for x in np.arange(0, 1, 0.01):
 
-            k_step = x*vectors([1,1]) + k_last
-            #k_step = x*vectors([1,1])
+            k_step = x*vectors.v + k_last
+
+            # for plotting the path in the brillouin zone
             Path_x = np.append(Path_x, k_step[0]/RLC)
             Path_y = np.append(Path_y, k_step[1]/RLC)
 
-            Path = np.append(Path, x*vector_length + Path_Offset)
-            eValues, eVectors = np.linalg.eig(Hamiltonian(k_step))
-            np.sort(eValues)
-
+            Path = np.append(Path, x*vectors.len() + Path_Offset)
+            #eValues, eVectors = np.linalg.eig(Hamiltonian(k_step)) #evalues and evectors (slow)
+            eValues = np.linalg.eigvalsh(Hamiltonian(k_step))   #just evalues
+            eValues.sort()
+            
             for i in np.arange(0, 6, 1):
-                #if (count!=0) and (np.absolute(eValues[i].real - Energy[i][-1]) > 0.2):
-                #    eValues[i] = Energy[i][-1]
+                    Energy[i] = np.append(Energy[i], eValues[i].real)
+                    #EigenVectors[i] = np.append(EigenVectors[i], eVectors[i], axis = 1)
 
-                Energy[i] = np.append(Energy[i], eValues[i].real)
-
-        k_last += vectors([1,1])
+        k_last += vectors.v
         print("\nnew k offset = ", k_last/RLC)
 
-        Path_Offset += vector_length
+        Path_Offset += vectors.len()
         print("new x offset = ", Path_Offset)
-
-        count += 1
 
     return Energy, Path, EigenVectors, Path_x, Path_y
 
 plot_y, plot_x, eigvec, path_x, path_y = Figure_d()
 
+# plot the path in k that is taken
 fig = plt.figure(figsize=(6,6))
-
 ax = fig.add_subplot(111)
 
-#ax.plot(path_x,
-#        path_y,
-#        marker = 'o')
+ax.plot(path_x,
+        path_y,
+        marker = 'o',
+        color = 'black',
+        markersize = 2)
 
-#plt.xlim(-2,2)
-#plt.ylim(-2,2)
+
+ax.plot(0,0, marker = 'o', color = 'red')
+ax.plot(Gamma_to_M.v[0]/RLC,Gamma_to_M.v[1]/RLC, marker = 'o', color = 'red')
+ax.plot(Gamma_to_K.v[0]/RLC,Gamma_to_K.v[1]/RLC, marker = 'o', color = 'red')
+ax.plot(Gamma_to_K_prime.v[0]/RLC,Gamma_to_K_prime.v[1]/RLC, marker = 'o', color = 'red')
+
+plt.axhline(y=0, xmin=-2, xmax=2, color = 'green')
+plt.axvline(x=0, ymin=-2, ymax=2, color = 'green')
+
+plt.xlim(-2,2)
+plt.ylim(-2,2)
+
+plt.show()
 
 
+# plot the eigenvalues (electronic bands)
+fig = plt.figure(figsize=(6,6))
+ax = fig.add_subplot(111)
 
 for i in np.arange(0,6,1):
-    if i < 3:
+    if (i % 2) == 0:
         colour = 'red'
     else:
         colour = 'blue'
