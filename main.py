3643e8fd7a45259bdd40d68c41d5f32c9ed8b498
C-Birkett <conanbirkett@gmail.com>
2021-11-15 16:10:39 +0000

    15/11 moved lattice generation & finding evalues etc to new lattice class

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # This is the main file for this project
 
 import numpy as np
-from scipy import linalg as scipyLinalg
 import cmath
 import math
 import matplotlib.pyplot as plt
@@ -145,18 +144,10 @@ sigma_0 = np.array([[1,0],
 sigma_z = np.array([[1,0],
                     [0,-1]])
 
-test_array = np.array([[1,2,3],
-                        [4,5,6],
-                        [7,8,9]])
-
-#print(np.kron(sigma_0,test_array))
-
 # --- Hamiltonian ---
 
 def Hamiltonian(k):
     return np.kron(sigma_0, Hamiltonian_nearest_neighbors(k)) + np.kron(sigma_z, (1/2)*lambda_SOC*L_z)
-
-#print(Hamiltonian([1,1]))
 
 #TODO define some sort of eigenvector, do across range of k values that are on paths K M, Gamma etc
 
@@ -171,9 +162,6 @@ def Hamiltonian(k):
 # gamma -> m = 1/2 b (b = reciprocal lattice constant)
 # gamma -> k = b/sqrt(3)
 # k -> m = b/2*sqrt(3)
-
-# reciprocal lattice constant
-RLC = (4*np.pi)/(np.sqrt(3)*PLC)
 
 # 2D rotation matrix
 def rotation_matrix(theta):
@@ -221,201 +209,211 @@ print ("\n rotated test vectors: ", test_vector_1.v, test_vector_2.v)
 print ("\n evaluate test vectors at [0.5,0.5]: ", test_vector_1.k([0.5,0.5]), test_vector_2.k([0.5,0.5]))
 """
 
-
-""" backup of old lattice vectors (as functions rather than Vector class)
-# Primitive lattice vectors
-def PLV_1(k: np.array):
-    return np.array([np.sqrt(3)*PLC*k[0]/2, PLC*k[1]/2])
-
-def PLV_2(k: np.array):
-    return np.array([-np.sqrt(3)*PLC*k[0]/2, PLC*k[1]/2])
-
-# Reciprocal lattice vectors
-def RLV_1(k: np.array):
-    return np.array([2*np.pi*k[0]/PLC, 2*np.pi*k[1]/(np.sqrt(3)*PLC)])
-
-def RLV_2(k: np.array):
-    return np.array([2*np.pi*k[0]/PLC, -2*np.pi*k[1]/(np.sqrt(3)*PLC)])
-
-print("PLC = ", PLC, "\nRLC = ", RLC)
-print("|PLV| = ", np.linalg.norm(PLV_1([1,1])), "\n|RLV| = ", np.linalg.norm(RLV_1([1,1])))
-"""
-
-# TODO - make a lattice class, then can do lattice.generate_vectors(), lattice.rotate_vectors() etc
-# can even do lattice.find_eigenvalues(path) etc
-# this will make layering and rotating lattices easier. Can even extend functionality to superlattices.
-# for now this will be useful for adding the 2nd layer and then rotating it
-
 # for simplicity we will define the "default" primitive and reciprocal lattice vectors the same way they were defined before (before weekend of 13/11/21), this can be easily changed in the future if need be
 
 # Primitive lattice vectors
 PLV_1 = Vector([np.sqrt(3)*PLC/2, PLC/2])
 PLV_2 = Vector([-np.sqrt(3)*PLC/2, PLC/2])
 
+"""
 # Reciprocal lattice vectors
 RLV_1 = Vector([2*np.pi/PLC, 2*np.pi/(np.sqrt(3)*PLC)])
 RLV_2 = Vector([2*np.pi/PLC, -2*np.pi/(np.sqrt(3)*PLC)])
-
-# Rotate the reciprocal lattice here:
-RLV_1.rotate90(0)
-RLV_2.rotate90(0)
-
-# testing with weird angles
-#RLV_1.rotate(5*np.pi/7)
-#RLV_2.rotate(5*np.pi/7)
-
-
-# Next we define the vectors that describe the paths for the brillouin zone as functions of k
-
-Gamma_to_M = Vector((1.0/2.0)*RLV_1.v)
-
-Gamma_to_K_prime = Vector((1.0/3.0) * (RLV_1.v + RLV_2.v))
-
-M_to_K_prime = Vector((-1.0/6.0)*RLV_1.v + (1.0/3.0)*RLV_2.v)
-
-M_to_K = Vector((1.0/6.0)*RLV_1.v - (1.0/3.0)*RLV_2.v)
-
-#to avoid rotating twice
-K_prime_to_M = Vector(M_to_K.v)
-
-Gamma_to_K = Vector((2.0/3.0)*RLV_1.v - (1.0/3.0)*RLV_2.v)
-
-K_to_Gamma = Vector((-2.0/3.0)*RLV_1.v + (1.0/3.0)*RLV_2.v)
-
-M_to_M = [M_to_K, K_to_Gamma, Gamma_to_K_prime, K_prime_to_M]
-
-"""
-# test these make a closed loop - returns 10-7 error in x, which is negligible
-tmp = np.array([0.0,0.0])
-print("\ntmp = ", tmp)
-for v in M_to_M:
-    tmp += v.v
-    print("\nvector = ", v.v)
-    print("\ntmp = ", tmp)
-
-print("this should be equal to zero: ", tmp)
 """
 
-# conditions ai dot bj = 2Pi delta(i,j)
-# pairwise parallel or perpendicular
+# Class that describes a lattice, can generate reciprocal lattice.
+# TODO migrate Hamiltonian generation into this class &/or make a Hamiltonian class that is able to generate larger & larger hamiltonians to then find the corresponding evalues from
+class Lattice:
+    def __init__(this, plc, plv_1: Vector, plv_2: Vector, angle):
+        #primitive lattice constant
+        print("\n= Generating lattice... =")
+        this.PLC = plc
+        #primitive lattice vectors
+        this.PLV_1 = plv_1
+        this.PLV_2 = plv_2
 
-# define the reciprocal lattice constant
+        print("\nLattice constant = ", this.PLC)
+        print("Lattice vectors = ", this.PLV_1.v, this.PLV_2.v)
 
-# define better transform from real to reciprocal
+        if angle != 0:
+            this.PLV_1.rotate(angle)
+            this.PLV_2.rotate(angle)
+            print("Primitive lattice vectors rotated by ", angle, " radians to ", this.PLV_1, this.PLV_2)
 
-# find coordinates of M, K, K'
+    def gen_reciprocal_lattice(this):
+        print("\n=Generating reciprocal lattice...=")
+        # reciprocal lattice constant
+        this.RLC = (4*np.pi)/(np.sqrt(3)*this.PLC)
+        # reciprocal lattice vectors
+        # conditions to generate RLVs: ai dot bj = 2Pi delta(i,j)
 
+        four_pi_over_rt_three_RLC_sqrd = (4*np.pi)/(np.sqrt(3)*this.PLC*this.PLC)
+        this.RLV_1 = Vector(four_pi_over_rt_three_RLC_sqrd*this.PLV_1.v)
+        this.RLV_2 = Vector(-four_pi_over_rt_three_RLC_sqrd*this.PLV_2.v)
+        print("\nReciprocal lattice constant = ", this.RLC)
+        print("Reciprocal lattice vectors = ", this.RLV_1.v, this.RLV_2.v)
+        
+    # Define the vectors that describe the paths for points Gamma, K, K', M in the brillouin zone
+    def gen_brilloin_zone_vectors(this):
+        this.Gamma_to_M = Vector((1.0/2.0)*this.RLV_1.v)
 
-def Figure_d():
+        this.Gamma_to_K_prime = Vector((1.0/3.0) * (this.RLV_1.v + this.RLV_2.v))
 
-    # this is not great but it doesn't work otherwise (numpy)
-    Energy_1 = np.array([])
-    Energy_2 = np.array([])
-    Energy_3 = np.array([])
-    Energy_4 = np.array([])
-    Energy_5 = np.array([])
-    Energy_6 = np.array([])
-    Energy = [Energy_1,Energy_2,Energy_3,Energy_4,Energy_5,Energy_6]
+        this.M_to_K_prime = Vector((-1.0/6.0)*this.RLV_1.v + (1.0/3.0)*this.RLV_2.v)
+
+        this.M_to_K = Vector((1.0/6.0)*this.RLV_1.v - (1.0/3.0)*this.RLV_2.v)
+
+        #to avoid rotating twice
+        this.K_prime_to_M = Vector(this.M_to_K.v)
+
+        this.Gamma_to_K = Vector((2.0/3.0)*this.RLV_1.v - (1.0/3.0)*this.RLV_2.v)
+
+        this.K_to_Gamma = Vector((-2.0/3.0)*this.RLV_1.v + (1.0/3.0)*this.RLV_2.v)
+
+    def gen_brilloin_zone_path(this):
+        this.M_to_M = [this.M_to_K,
+                        this.K_to_Gamma,
+                        this.Gamma_to_K_prime,
+                        this.K_prime_to_M]
+        """
+        # test these make a closed loop - returns 10-7 error in x, which is negligible
+        tmp = np.array([0.0,0.0])
+        print("\ntmp = ", tmp)
+        for v in M_to_M:
+            tmp += v.v
+            print("\nvector = ", v.v)
+            print("\ntmp = ", tmp)
+
+        print("this should be equal to zero: ", tmp)
+        """
+
+    def get_eigenvalues(this):
+        # this is not great but it doesn't work otherwise (numpy)
+        Energy_1 = np.array([])
+        Energy_2 = np.array([])
+        Energy_3 = np.array([])
+        Energy_4 = np.array([])
+        Energy_5 = np.array([])
+        Energy_6 = np.array([])
+        Energy = [Energy_1,Energy_2,Energy_3,Energy_4,Energy_5,Energy_6]
     
-    EVec1 = np.array([[]])
-    EVec2 = np.array([[]])
-    EVec3 = np.array([[]])
-    EVec4 = np.array([[]])
-    EVec5 = np.array([[]])
-    EVec6 = np.array([[]])
-    EigenVectors = [EVec1, EVec2, EVec3, EVec4, EVec5, EVec6]
+        EVec1 = np.array([[]])
+        EVec2 = np.array([[]])
+        EVec3 = np.array([[]])
+        EVec4 = np.array([[]])
+        EVec5 = np.array([[]])
+        EVec6 = np.array([[]])
+        EigenVectors = [EVec1, EVec2, EVec3, EVec4, EVec5, EVec6]
 
-    Path_x = np.array([])
-    Path_y = np.array([])
-    Path = np.array([])
+        Path_x = np.array([])
+        Path_y = np.array([])
+        Path = np.array([])
 
-    Path_Offset = 0 #for plotting x axis
-    #assuming starting from point M - should rotate with the rest
-    k_last = Gamma_to_M.v
-    #k_last = Gamma_to_M.v
-    print("\nstarting k offset (M) = ", k_last/RLC)
-    Path_Offset += np.linalg.norm(k_last)
-    print("\ninitial x offset = ", Path_Offset)
+        Path_Offset = 0 #for plotting x axis
+        #assuming starting from point M - should rotate with the rest
+        k_last = this.Gamma_to_M.v
+        #k_last = Gamma_to_M.v
+        print("\nstarting k offset (M) = ", k_last/this.RLC)
+        Path_Offset += np.linalg.norm(k_last)
+        print("\ninitial x offset = ", Path_Offset)
 
-    for vectors in M_to_M:
-        print("\nmoving along vector: ", vectors.v)
-        for x in np.arange(0, 1, 0.01):
+        for vectors in this.M_to_M:
+            print("\nmoving along vector: ", vectors.v)
+            for x in np.arange(0, 1, 0.01):
 
-            k_step = x*vectors.v + k_last
+                k_step = x*vectors.v + k_last
 
-            # for plotting the path in the brillouin zone
-            Path_x = np.append(Path_x, k_step[0]/RLC)
-            Path_y = np.append(Path_y, k_step[1]/RLC)
+                # for plotting the path in the brillouin zone
+                Path_x = np.append(Path_x, k_step[0]/this.RLC)
+                Path_y = np.append(Path_y, k_step[1]/this.RLC)
 
-            Path = np.append(Path, x*vectors.len() + Path_Offset)
-            #eValues, eVectors = np.linalg.eig(Hamiltonian(k_step)) #evalues and evectors (slow)
-            eValues = np.linalg.eigvalsh(Hamiltonian(k_step))   #just evalues
-            eValues.sort()
+                Path = np.append(Path, x*vectors.len() + Path_Offset)
+                #eValues, eVectors = np.linalg.eig(Hamiltonian(k_step)) #evalues and evectors (slow)
+                eValues = np.linalg.eigvalsh(Hamiltonian(k_step))   #just evalues
+                eValues.sort()
             
-            for i in np.arange(0, 6, 1):
+                for i in np.arange(0, 6, 1):
                     Energy[i] = np.append(Energy[i], eValues[i].real)
                     #EigenVectors[i] = np.append(EigenVectors[i], eVectors[i], axis = 1)
 
-        k_last += vectors.v
-        print("\nnew k offset = ", k_last/RLC)
+            k_last += vectors.v
+            print("\nnew k offset = ", k_last/this.RLC)
 
-        Path_Offset += vectors.len()
-        print("new x offset = ", Path_Offset)
+            Path_Offset += vectors.len()
+            print("new x offset = ", Path_Offset)
 
-    return Energy, Path, EigenVectors, Path_x, Path_y
+        this.eValues = Energy
+        this.eVectors = EigenVectors
+        this.path = Path
+        this.path_x = Path_x
+        this.path_y = Path_y
 
-plot_y, plot_x, eigvec, path_x, path_y = Figure_d()
+        return Energy, Path, EigenVectors, Path_x, Path_y
+        
+    # plot the path in k that is taken
+    def plot_brillouin_zone_path(this):
+        fig = plt.figure(figsize=(6,6))
+        ax = fig.add_subplot(111)
 
-# plot the path in k that is taken
-fig = plt.figure(figsize=(6,6))
-ax = fig.add_subplot(111)
-
-ax.plot(path_x,
-        path_y,
-        marker = 'o',
-        color = 'black',
-        markersize = 2)
-
-
-ax.plot(0,0, marker = 'o', color = 'red')
-ax.plot(Gamma_to_M.v[0]/RLC,Gamma_to_M.v[1]/RLC, marker = 'o', color = 'red')
-ax.plot(Gamma_to_K.v[0]/RLC,Gamma_to_K.v[1]/RLC, marker = 'o', color = 'red')
-ax.plot(Gamma_to_K_prime.v[0]/RLC,Gamma_to_K_prime.v[1]/RLC, marker = 'o', color = 'red')
-
-plt.axhline(y=0, xmin=-2, xmax=2, color = 'green')
-plt.axvline(x=0, ymin=-2, ymax=2, color = 'green')
-
-plt.xlim(-2,2)
-plt.ylim(-2,2)
-
-plt.show()
+        ax.plot(this.path_x,
+                this.path_y,
+                marker = 'o',
+                color = 'black',
+                markersize = 2)
 
 
-# plot the eigenvalues (electronic bands)
-fig = plt.figure(figsize=(6,6))
-ax = fig.add_subplot(111)
+        ax.plot(0,0, marker = 'o', color = 'red')
+        ax.plot(this.Gamma_to_M.v[0]/this.RLC,this.Gamma_to_M.v[1]/this.RLC, marker = 'o', color = 'red')
+        ax.plot(this.Gamma_to_K.v[0]/this.RLC,this.Gamma_to_K.v[1]/this.RLC, marker = 'o', color = 'red')
+        ax.plot(this.Gamma_to_K_prime.v[0]/this.RLC,this.Gamma_to_K_prime.v[1]/this.RLC, marker = 'o', color = 'red')
 
-for i in np.arange(0,6,1):
-    if (i % 2) == 0:
-        colour = 'red'
-    else:
-        colour = 'blue'
+        plt.axhline(y=0, xmin=-2, xmax=2, color = 'green')
+        plt.axvline(x=0, ymin=-2, ymax=2, color = 'green')
 
-    ax.plot(plot_x,
-            plot_y[i].real,
-            #xerr = ,
-            #yerr = ,
-            #capsize = ,
-            #marker = 'o',
-            #markersize = 2,
-            color = colour,
-            markerfacecolor = 'black',
-            #linestyle = '-',
-            #label = 'asdef'
-            )
+        plt.xlim(-2,2)
+        plt.ylim(-2,2)
 
-plt.xlim(0,RLC*10)
-plt.xlim(plot_x[0],plot_x[-1])
-plt.ylim(-1,4)
+        plt.show()
 
-plt.show()
+    # plot the eigenvalues (electronic bands)
+    def plot_eigenvalues(this):
+        fig = plt.figure(figsize=(6,6))
+        ax = fig.add_subplot(111)
+
+        for i in np.arange(0,6,1):
+            if (i % 2) == 0:
+                colour = 'red'
+            else:
+                colour = 'blue'
+
+            ax.plot(this.path,
+                    this.eValues[i].real,
+                    #xerr = ,
+                    #yerr = ,
+                    #capsize = ,
+                    #marker = 'o',
+                    #markersize = 2,
+                    color = colour,
+                    markerfacecolor = 'black',
+                    #linestyle = '-',
+                    #label = 'asdef'
+                    )
+
+        plt.xlim(0,this.RLC*10)
+        plt.xlim(this.path[0],this.path[-1])
+        plt.ylim(-1,4)
+
+        plt.show()
+
+
+# Test lattices from start to finish
+
+myLattice = Lattice(PLC, PLV_1, PLV_2, np.pi/4)   #no rotation, defaults
+
+myLattice.gen_reciprocal_lattice()
+myLattice.gen_brilloin_zone_vectors()
+myLattice.gen_brilloin_zone_path()
+myLattice.get_eigenvalues()
+
+myLattice.plot_brillouin_zone_path()
+myLattice.plot_eigenvalues()
